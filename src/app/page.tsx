08c4ad712289aca3e426d220f812cf18746acd94
samiursakib <rafisamiur@gmail.com>
2024-12-09 23:09:13 +0600

    responsive layout completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import Main from "@/components/Main";
+import Sidebar from "@/components/Sidebar";
+import { useState } from "react";
+
 export default function Home() {
-  return <div>Home</div>;
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div className="flex">
+      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      {isSidebarOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-40 md:hidden"
+          onClick={toggleSidebar}
+        ></div>
+      )}
+      <Main toggleSidebar={toggleSidebar} />
+    </div>
+  );
 }
